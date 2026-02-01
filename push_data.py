8b20b060baa4fbcfd5a2f55f6cafd0f8db406fef
Aastha Rathore <rathoreaasthaspare@gmail.com>
2026-02-02 00:20:00 +0530

    COnnected to MongoDB

---
+++ push_data.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import json
+from dotenv import load_dotenv
+import certifi
+load_dotenv()
+uri = os.getenv("MONGODB_URL")
+
+
+ca=certifi.where()
+
+
+import pandas as pd
+import numpy as np
+import pymongo
+from Network_Security.exception.exception import NetworkSecurityException
+from Network_Security.logging.logger import logging
+
+class NetworkDataExtract():
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+
+            # data.T is transpose of data 
+            # .values function creates array of these data 
+
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+            self.database=database
+            self.collection=collection
+            self.records=records
+
+            self.mongo_client = pymongo.MongoClient(
+                uri,
+                tls=True,
+                tlsCAFile=certifi.where()
+            )
+
+            self.database = self.mongo_client[self.database]
+            
+            self.collection=self.database[self.collection]
+
+            self.collection.insert_many(self.records)
+
+            return(len(self.records))
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+if __name__=='__main__':
+    FILE_PATH="Network_Data\phisingData.csv"
+    DATABASE="rathoreaastha1510_db_user"
+    Collection="NetworkData"
+    networkobj=NetworkDataExtract()
+    records=networkobj.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=networkobj.insert_data_mongodb(records,DATABASE,Collection)
+    print(no_of_records)
+        
+
+
