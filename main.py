a5264943560823494b59c049017580d8bff8da4d
Aastha Rathore <rathoreaasthaspare@gmail.com>
2026-03-12 23:58:01 +0530

    adding data ingestion , validation and transformation files.

---
+++ main.py
@@ -0,0 +1,36 @@
+from Network_Security.components.data_ingestion import DataIngestion
+from Network_Security.components.data_validation import DataValidation
+from Network_Security.components.data_tranformation import DataTransformation
+from Network_Security.exception.exception import NetworkSecurityException
+from Network_Security.logging.logger import logging
+from Network_Security.entity.config_entity import DataIngestionConfig  , DataValidationConfig, DataTransformationConfig
+from Network_Security.entity.config_entity import TrainingPipelineConfig
+
+import sys
+
+if __name__=="__main__":
+    try:
+        training=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(training)
+        data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info("Initiating the data ingestion")
+        data_ingestion_artifact=data_ingestion.initiate_data_ingestion() 
+        print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training)
+        data_validation=DataValidation(data_ingestion_artifact , data_validation_config)
+        logging.info("Initiating the data validation")
+        data_validation_artifacts=data_validation.initiate_data_validation()
+        logging.info("data validation done")
+        logging.info("Initiating the data tranformation")
+
+        data_tranformation_config=DataTransformationConfig(training)
+        data_transformation=DataTransformation(data_validation_artifacts, data_tranformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("data tranformation done")
+        print(data_transformation_artifact)
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+
+        
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 """
-Setup.py files are important for packaging and distributing Python projects. It is used by setuptools to define the configuration of your porject. They contain metadata, dependencies,etc about the project and instructions on how to install it.
+Setup.py files are important for packaging and distributing Python projects. It is used by setuptools to define the configuration of your project. They contain metadata, dependencies,etc about the project and instructions on how to install it.
 
 Any python package that is installed or imported has its own setup file for its metadata, versions etc.
 
-find_packages -> searches ofr folder having "__init__.py" file and considers them as packages. 
+find_packages -> searches for folder having "__init__.py" file and considers them as packages. 
 
 -e. -> When we run (pip install -r requirements.txt), python reads all the packages mentioned in req.txt file and as soons as it cooms to (-e.) , it is redirected to setup.py file and this file is now executed where it ignores (-e.) and continues with rest of the installation.
 
